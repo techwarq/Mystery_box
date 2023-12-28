e43438701be9eddef96f43e5ed7a2f3b7e1a1b98
Sonali Nayak <102250604+techwarq@users.noreply.github.com>
2023-12-29 00:42:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,8 +25,11 @@ async function generateQRCode() {
 
             // Hide the textarea and show the box with the QR code
             document.querySelector('.promo textarea').style.display = 'none';
-            document.querySelector('.submit').style.display = 'none';
             qrCodeBox.style.display = 'block';
+
+            // Generate a shareable link and update the URL
+            const shareableLink = `${window.location.origin}${window.location.pathname}?message=${encodeURIComponent(message)}`;
+            window.history.pushState({ path: shareableLink }, '', shareableLink);
         } catch (error) {
             console.error('Error generating QR code:', error);
             alert('Error generating QR code. Please try again.');
@@ -35,3 +38,15 @@ async function generateQRCode() {
         alert('Please enter a message before submitting.');
     }
 }
+
+// Check if there's a message in the URL on page load
+window.onload = function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const messageParam = urlParams.get('message');
+
+    if (messageParam) {
+        // If a message is found in the URL, simulate a click on the submit button
+        document.getElementById('messageInput').value = decodeURIComponent(messageParam);
+        generateQRCode();
+    }
+};
